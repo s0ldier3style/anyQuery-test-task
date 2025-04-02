b37a6852840e2ff65f625e3a2dcf2802a9a066f2
Vadim Malinovskii <overrated322@mail.ru>
2025-04-02 16:44:45 +0300

    initial commit

---
+++ src/js/data.js
@@ -0,0 +1,18 @@
+const products = [
+  {
+    label: "Хит продаж",
+    image: "../svg/cube-shop-card.svg",
+    discount: "25%",
+    brand: "Бренд",
+    title:
+      "Полное название товара в несколько строк для вида с обрывом в конце...",
+    price: "5 990 ₽",
+    oldPrice: "5 990 ₽",
+  },
+];
+
+for (let i = 1; i < 14; i++) {
+  products.push({ ...products[0] });
+}
+
+export default products;
